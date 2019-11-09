bf199a9ae02c344a66aa21b949f2676f2f9f6f57
wailo <wailo_2@hotmail.com>
2019-11-09 14:12:04 +0800

    Added vue-router with routing functionality

---
+++ src/views/demo-view.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="demo">
+    <h1>This is demo page page</h1>
+    <Demo/>
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import Demo from '@/components/Demo.vue'
+
+export default {
+  name: 'demo-view',
+  components: {
+    Demo
+  }
+}
+</script>
