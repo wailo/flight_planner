882dd957ef59947ebff2da3c68db1e14d224569e
wailo <wailo_2@hotmail.com>
2019-11-12 20:40:51 +0800

    Demo works with updating links

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,68 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/#d1">Home</router-link> |
-      <router-link to="/#d2">Demo</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <link rel="stylesheet" type="text/css" href="fullpage.css" />
+    <full-page ref="fullpage" :options="options" navigation="true" id="fullpage">
+      <div class="section">
+        <h1>Just Title</h1>
+      </div>
+
+      <div class="section">
+        <h1>Navigator</h1>
+        <p>A descision making aid tool for pilot and airlines</p>
+      </div>
+
+      <div class="section">
+        <h1>What is it</h1>
+        <ul>
+          <li>These points are subject to disruption due to weather, closures, diversion etc..</li>
+          <li>When a disruption happens, The pilot/airline are not informed until approaching the point, which is leads to diversions that is not optimal</li>
+          <li>SWIM navigator provides a mean to detect this changes using external data sources and inform the pilot/airlines ahead of time, or provide and alternatives.</li>
+        </ul>
+      </div>
+      <div class="section">
+        <h1>Demo</h1>
+        <Demo />
+      </div>
+    </full-page>
   </div>
 </template>
 
+<script>
+import HelloWorld from "@/components/HelloWorld";
+import Demo from "@/components/Demo";
+export default {
+  name: "App",
+  components: { HelloWorld, Demo },
+  data() {
+    return {
+      options: {
+        menu: "#menu",
+        anchors: ["page1", "page2", "page3", "page4"],
+        sectionsColor: ["#202020", "#202020", "#202020", "#202020"]
+      }
+    };
+  }
+}; 
+</script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  margin: 0;
+  padding: 0;
+}
+.section {
   text-align: center;
-  color: #2c3e50;
+  color: aliceblue;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
 }
 
-#nav {
-  padding: 30px;
+h1 {
+    font-size: 6em;
+    text-align: center;
+    color: #fff;
+    font-weight: 700;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,31 @@
 <template>
-  <div class="home">
+
+  <div class="home">    
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld id='d1' msg="Welcome to Your Vue.js App"/>
     <h1 id='d2'>Demo</h1>
     <Demo> </Demo>
+  
   </div>
 </template>
+
+
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import Demo from '@/components/Demo.vue'
+
 export default {
   name: 'home',
   components: {
     HelloWorld,
-    Demo
+    Demo,
   }
 }
 </script>
+
+<style scoped>
+
+
+</style>
