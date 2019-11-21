9a09d6ea22a479a7a2d305b8fd40876f8e75877f
wailo <wailo_2@hotmail.com>
2019-11-21 17:42:58 +0800

    Added markers on the map with links

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,7 +4,7 @@
 
 <script>
 import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
-
+import nodes from "@/assets/waypoints.json";
 export default {
   name: "MyAwesomeMap",
   components: {
@@ -12,12 +12,42 @@ export default {
     LTileLayer,
     LMarker
   },
+  data: function() {
+    return {
+      nodes: nodes["nodes"],
+      map: null,
+      icon: L.icon({
+        iconUrl: "https://img.icons8.com/plasticine/50/000000/marker.png",
+        iconSize: [20, 20] // size of the icon
+        // iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+        //  shadowAnchor: [4, 62], // the same for the shadow
+        // popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+      })
+    };
+  },
+  methods: {
+    create_marker: function(node) {
+      L.marker([node["lat"], node["lon"]], { icon: this.icon })
+        .addTo(this.map)
+        .bindPopup(node["name"]);
+    },
+    create_link: function(start_node, end_node) {
+      var latlngs = [
+        [start_node.lat, start_node.lon],
+        [end_node.lat, end_node.lon]
+      ];
+      var polyline = L.polyline(latlngs, { color: "red" }).addTo(this.map);
+      // zoom the map to the polyline
+      this.map.fitBounds(polyline.getBounds());
+    }
+  },
   mounted() {
     this.$nextTick(() => {
-      this.map = this.$refs.myMap.mapObject.setView([38.63, -90.23], 12);
-      //this.map = L.map("myMap").setView([38.63, -90.23], 12);
+      this.map = this.$refs.myMap.mapObject.setView([52.3, 4.76], 8);
+
       this.tileLayer = L.tileLayer(
-        "https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png",
+        "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
+        // "https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png",
         {
           maxZoom: 18,
           attribution:
@@ -25,6 +55,12 @@ export default {
         }
       );
       this.tileLayer.addTo(this.map);
+      this.nodes.forEach(node => {
+        this.create_marker(node);
+      });
+      for (var i = 1; i < this.nodes.length; ++i) {
+        this.create_link(this.nodes[i - 1], this.nodes[i]);
+      }
     });
   }
 };
@@ -32,6 +68,6 @@ export default {
 
 <style scoped>
 .map {
-  height: 300px;
+  height: 600px;
 }
 </style>
