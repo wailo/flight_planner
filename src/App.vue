9acff5d46ccef3efce55e423a7c805ff3564e709
wailo <wailo_2@hotmail.com>
2019-11-21 10:56:27 +0800

    Added leaflet map component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
   <div class="app">
     <h1>Demo</h1>
-        <Demo/>
+    <Map />
+    <Demo />
   </div>
 </template>
 
 <script>
 import HelloWorld from "@/components/HelloWorld";
 import Demo from "@/components/Demo";
+import Map from "@/components/Map";
 export default {
   name: "App",
-  components: { HelloWorld, Demo },
+  components: { HelloWorld, Demo, Map },
   data() {
-    return {
-
-    };
+    return {};
   }
-}; 
+};
 </script>
-<style>
-
-.h1 {
+<style scoped>
+h1 {
   color: white;
 }
 .app {
-  background: rgba(27, 27, 27, 0.932)
-
+  background: rgba(27, 27, 27, 0.932);
 }
+
 * {
   margin: 0;
   padding: 0;
---
+++ src/components/Map.vue
@@ -0,0 +1,37 @@
+<template>
+  <l-map ref="myMap" class="map"></l-map>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+
+export default {
+  name: "MyAwesomeMap",
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.map = this.$refs.myMap.mapObject.setView([38.63, -90.23], 12);
+      //this.map = L.map("myMap").setView([38.63, -90.23], 12);
+      this.tileLayer = L.tileLayer(
+        "https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png",
+        {
+          maxZoom: 18,
+          attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+        }
+      );
+      this.tileLayer.addTo(this.map);
+    });
+  }
+};
+</script>
+
+<style scoped>
+.map {
+  height: 300px;
+}
+</style>
